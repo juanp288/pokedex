673b5c02c9fa7641d68daeb052dbd9cb7cf62837
Juan Pablo Victoria <jpv288@gmail.com>
2023-11-29 18:23:28 -0500

    Section 8 - Ep92
    92. Realizar petición http desde Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     CommonModule,
+    SeedModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
