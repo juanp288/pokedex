99776bf953a0bb407768480f95a3483a138675e3
Juan Pablo Victoria <jpv288@gmail.com>
2023-11-29 19:03:14 -0500

    Section 8 - Ep 96
    96. Crear un custom provider - opcional - AxiosAdapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
   private readonly pokeApi = 'https://pokeapi.co/api/v2/pokemon?limit=10';
 
-  constructor(private readonly pokeService: PokemonService) {}
+  constructor(
+    private readonly pokeService: PokemonService,
+    private readonly axiosAdapter: AxiosAdapter,
+  ) {}
 
   async seed() {
-    const { data } = await this.axios.get<PokeResponse>(this.pokeApi);
+    const data = await this.axiosAdapter.get<PokeResponse>(this.pokeApi);
 
     await this.pokeService.truncate();
 
